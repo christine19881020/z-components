401b404f068667b0ce595ea3c9ebbcf3a2bf00fc
张丹鸯 <zdy_19881020@163.com>
2020-07-07 19:31:35 +0800

    update version

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import DefineVue from './components/defineVue/defineVue.vue'
+import AddressChoose from './components/addressChoose/addressChoose.vue'
 
 module.exports = {
   install: function(Vue, options) {
     Vue.component('df-vue', DefineVue);
+    Vue.component('address-choose', AddressChoose);
   }
 }
